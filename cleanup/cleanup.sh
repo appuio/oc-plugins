ba134a4a862bf8f03768ca2f748eb5daaceb0767
Peter Bittner <peter.bittner@vshn.ch>
2018-10-30 16:54:01 +0100

    Add IMAGE argument, filter image tags properly

--- cleanup/cleanup.sh
+++ cleanup/cleanup.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+IMAGE_NAME=$1
 
 set -e -o pipefail -u
 
 GIT_REPO_PATH=${KUBECTL_PLUGINS_LOCAL_FLAG_GIT_REPO_PATH}
 FORCE=${KUBECTL_PLUGINS_LOCAL_FLAG_FORCE}
 KEEP=${KUBECTL_PLUGINS_LOCAL_FLAG_KEEP}
-FROM_HEAD=$(( KEEP+1 ))
 NAMESPACE=${KUBECTL_PLUGINS_CURRENT_NAMESPACE}
 oc="${KUBECTL_PLUGINS_CALLER} -n ${NAMESPACE}"
 
@@ -14,7 +15,13 @@ get_commit_list() {
     git -C ${GIT_REPO_PATH} rev-list --author-date-order HEAD | tail -n "+2"
 }
 
-get_active_images() {
+get_imagestreamtags() {
+    local imagestream_path='{range .status.tags[*]}{.tag}{"\n"}{end}'
+
+    ${oc} get imagestream ${IMAGE_NAME} -o jsonpath="${imagestream_path}" | sort --unique
+}
+
+get_active_imagestreamtags() {
     local dc_rc_sfs_path='{range .items[*]}{.spec.template.spec.containers[*].image}{"\n"}{end}'
     local pod_cont_path='{range .items[*]}{.spec.containers[*].image}{"\n"}{end}'
     local pod_init_path='{range .items[*]}{.spec.initContainers[*].image}{"\n"}{end}'
@@ -28,46 +35,54 @@ get_active_images() {
     echo "${images_in_dc_rc_sset}" \
          "${images_in_pod_cont}" \
          "${images_in_pod_init}" \
-         "${images_in_cron}" | sort --unique
+         "${images_in_cron}" \
+         | xargs | tr ' ' '\n' \
+         | grep "/${IMAGE_NAME}:" \
+         | sed "s#.*/${NAMESPACE}/${IMAGE_NAME}:##" \
+         | sort --unique
 }
 
-get_inactive_images() {
-    local istags_path='{range .items[*]}{.metadata.name}{"\n"}{end}'
-    local sort_by_date='--sort-by .metadata.creationTimestamp'
-
-    local all_tags=$(${oc} get istag -o jsonpath="${istags_path}" ${sort_by_date} \
-                   | tail -n "+${FROM_HEAD}" | sort --unique)
-
-    local all_active=$(get_active_images)
+get_inactive_imagestreamtags() {
+    local all_tags=$(get_imagestreamtags)
+    local all_active=$(get_active_imagestreamtags)
 
     # all_inactive := all_tags - all_active
-    comm -23 <(echo ${all_tags}) <(echo ${all_active})
+    comm -23 \
+        <(echo ${all_tags} | tr ' ' '\n') \
+        <(echo ${all_active} | tr ' ' '\n')
 }
 
 get_deletion_candidates() {
     local commit_list=($(get_commit_list))
-    local inactive_images=($(get_inactive_images))
+    local inactive_istags=($(get_inactive_imagestreamtags))
     local deletion_candidates=()
 
     # select all inactive images tagged with one of the commits
-    for commit_hash in ${commit_list[*]}; do
-        for candidate in ${inactive_images[*]}; do
-            if [[ ${candidate} == *${commit_hash} ]]; then
-                deletion_candidates+=(${candidate})
+    for commit_hash in ${commit_list[@]}; do
+        for candidate in ${inactive_istags[@]}; do
+            if [[ ${candidate} == ${commit_hash} ]]; then
+                deletion_candidates+=("${IMAGE_NAME}:${candidate}")
             fi
         done
     done
 
-    echo ${deletion_candidates[*]}
+    # strip out the <KEEP> youngest images we want to keep
+    echo ${deletion_candidates[@]} | tr ' ' '\n' | tail -n "+$(( KEEP+1 ))"
 }
 
 main() {
+    test -z ${IMAGE_NAME} && {
+        echo 'Image name required. Please specify as first argument.'
+        echo
+        ${oc} get imagestream -o custom-columns="AVAILABLE IMAGES IN ${NAMESPACE}:{.metadata.name}"
+        exit 0
+    }
     test -z ${GIT_REPO_PATH} && {
         echo 'Path required. Please use -p to specify the Git repository.'
         exit 0
     }
 
-    echo "Comparing commits from ${GIT_REPO_PATH} in namespace ${NAMESPACE} .."
+    echo "Comparing commits from ${GIT_REPO_PATH} in namespace ${NAMESPACE}"
     local candidates=($(get_deletion_candidates))
     local candidate_count=${#candidates[@]}
 
@@ -76,17 +91,17 @@ main() {
         exit 0
     else
         echo "${candidate_count} image tags found for deletion:"
-        for image_tag in ${candidates[*]}; do
+        for image_tag in ${candidates[@]}; do
             echo "- $image_tag"
         done
     fi
 
     if [[ ${FORCE} != 'y' ]]; then
-        read -p "Delete images tags? (y/N) " FORCE
+        read -p "Delete ${candidate_count} image tags? (y/N) " FORCE
     fi
     if [[ ${FORCE} == 'y' ]]; then
         echo "Deleting ${candidate_count} image tags ..."
-        ${oc} delete istag --ignore-not-found ${candidates[*]}
+        ${oc} delete istag --ignore-not-found ${candidates[@]}
     else
         echo 'Nothing was deleted.'
     fi
